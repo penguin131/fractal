e4f34a6a7e84241bb9522b5820f6cd13f116bfbf
bootcamp <Zx746794>
2020-02-16 00:54:47 +0300

    first1

---
+++ cmake-build-debug/CMakeFiles/fractol.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/bootcamp/Desktop/C/fractol/src/calculation.c" "/Users/bootcamp/Desktop/C/fractol/cmake-build-debug/CMakeFiles/fractol.dir/src/calculation.c.o"
+  "/Users/bootcamp/Desktop/C/fractol/src/draw.c" "/Users/bootcamp/Desktop/C/fractol/cmake-build-debug/CMakeFiles/fractol.dir/src/draw.c.o"
+  "/Users/bootcamp/Desktop/C/fractol/src/formulas.c" "/Users/bootcamp/Desktop/C/fractol/cmake-build-debug/CMakeFiles/fractol.dir/src/formulas.c.o"
+  "/Users/bootcamp/Desktop/C/fractol/src/loop.c" "/Users/bootcamp/Desktop/C/fractol/cmake-build-debug/CMakeFiles/fractol.dir/src/loop.c.o"
+  "/Users/bootcamp/Desktop/C/fractol/src/main.c" "/Users/bootcamp/Desktop/C/fractol/cmake-build-debug/CMakeFiles/fractol.dir/src/main.c.o"
+  "/Users/bootcamp/Desktop/C/fractol/src/zoom.c" "/Users/bootcamp/Desktop/C/fractol/cmake-build-debug/CMakeFiles/fractol.dir/src/zoom.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../inc"
+  "../libft"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/fractol.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fractol.dir/src/calculation.c.o"
+  "CMakeFiles/fractol.dir/src/draw.c.o"
+  "CMakeFiles/fractol.dir/src/formulas.c.o"
+  "CMakeFiles/fractol.dir/src/loop.c.o"
+  "CMakeFiles/fractol.dir/src/main.c.o"
+  "CMakeFiles/fractol.dir/src/zoom.c.o"
+  "fractol"
+  "fractol.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fractol.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
